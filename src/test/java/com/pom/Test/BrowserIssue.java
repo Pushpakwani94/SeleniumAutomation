04c7fa779a435d0ae44fdcb04aa5bf2105763bdc
pushpak <wanipushpak76@gmail.com>
2023-09-02 22:38:19 +0530

    pushpak updating

---
+++ src/test/java/com/pom/Test/BrowserIssue.java
@@ -0,0 +1,21 @@
+package com.pom.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserIssue {
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://www.google.com");
+		
+	}
+
+}
